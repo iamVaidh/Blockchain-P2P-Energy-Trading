731aadf036d721a23c9b040a5911bec2e41494f1
Vaidh <1999prithviraj1999@gmail.com>
2023-05-02 18:40:47 +0100

    added missing events to contracts and fixed minor issues.

--- contracts/P2P.sol
+++ contracts/P2P.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity <=0.8.19;
 
 contract P2P {
     struct Prosumer {
@@ -24,10 +24,13 @@ contract P2P {
         uint amount,
         uint price
     );
+    event EnergyWithdrawn(address indexed sender, uint amount);
+    event Deposited(address indexed sender, uint amount);
 
     uint256 private price;
 
     function setPrice(uint256 newPrice) public {
+        require(newPrice > 0);
         price = newPrice;
     }
 
@@ -111,7 +114,6 @@ contract P2P {
     }
 
     uint256 public contractBalance;
-    event Deposited(address indexed sender, uint amount);
 
     function withdraw() public {
         require(getEnergyStatus(msg.sender) >= 0);
@@ -119,136 +121,17 @@ contract P2P {
         address payable sender = payable(msg.sender);
         uint amount = getEnergyStatus(sender);
 
+        if (amount > contractBalance) {
+            // Not enough energy in the contract to withdraw.
+            return;
+        }
+
+        require(amount <= prosumers[msg.sender].energyStatus);
+
         sender.transfer(amount);
+        contractBalance -= amount;
+        prosumers[msg.sender].energyStatus -= amount;
+
+        emit EnergyWithdrawn(msg.sender, amount);
     }
 }
-
-
-// Code above this is the ideal one.
-// pragma solidity ^0.8.19;
-
-// contract P2P {
-//     struct Prosumer {
-//         string name;
-//         address id;
-//         uint energyStatus;
-//         uint balance;
-//         uint reward;
-//     }
-
-//     mapping(address => Prosumer) prosumers;
-//     uint256 private price;
-
-//     event ProsumerRegistered(
-//         address indexed prosumer,
-//         string name,
-//         uint energyStatus,
-//         uint balance
-//     );
-    
-//     event EnergyTraded(
-//         address indexed buyer,
-//         address indexed seller,
-//         uint amount,
-//         uint price
-//     );
-
-//     event Deposited(address indexed sender, uint amount);
-
-//     function setPrice(uint256 newPrice) public {
-//         price = newPrice;
-//     }
-
-//     function registerProsumer(string memory name, uint256 energyStatus) public {
-//         require(bytes(name).length > 0);
-//         require(energyStatus >= 0);
-
-//         Prosumer storage newProsumer = prosumers[msg.sender];
-//         newProsumer.name = name;
-//         newProsumer.id = msg.sender;
-//         newProsumer.energyStatus = energyStatus;
-//         newProsumer.balance = 0;
-//         newProsumer.reward = 0;
-
-//         emit ProsumerRegistered(
-//             msg.sender,
-//             name,
-//             energyStatus,
-//             newProsumer.balance
-//         );
-//     }
-
-//     function buyEnergy(uint amount) public payable {
-//         require(price > 0);
-//         require(amount > 0);
-//         require(msg.value == price * amount);
-
-//         Prosumer storage buyer = prosumers[msg.sender];
-//         require(buyer.energyStatus >= amount);
-//         require(address(this).balance >= price * amount);
-
-//         Prosumer storage seller = prosumers[buyer.id];
-//         require(seller.energyStatus >= amount);
-
-//         buyer.energyStatus -= amount;
-//         seller.energyStatus += amount;
-//         buyer.balance += msg.value;
-//         seller.balance += (price * amount);
-
-//         // Reward the buyer and seller
-//         uint256 rewardAmount = (price * amount) / 100;
-//         buyer.reward += rewardAmount;
-//         seller.reward += rewardAmount;
-
-//         emit EnergyTraded(msg.sender, seller.id, amount, price);
-//     }
-
-//     function sellEnergy(uint amount) public {
-//         require(amount > 0);
-
-//         Prosumer storage seller = prosumers[msg.sender];
-//         require(seller.energyStatus >= amount);
-
-//         seller.energyStatus -= amount;
-//         seller.balance += (price * amount);
-//         seller.reward += (price * amount) / 100;
-
-//         emit EnergyTraded(seller.id, msg.sender, amount, price * amount);
-//     }
-
-//     function getEnergyStatus(address prosumer) public view returns (uint) {
-//         require(isProsumerRegistered(prosumer), "Prosumer is not registered");
-//         return prosumers[prosumer].energyStatus;
-//     }
-
-//     function getBalance(address prosumer) public view returns (uint) {
-//         require(isProsumerRegistered(prosumer), "Prosumer is not registered");
-//         return prosumers[prosumer].balance;
-//     }
-
-//     function getReward(address prosumer) public view returns (uint) {
-//         require(isProsumerRegistered(prosumer), "Prosumer is not registered");
-//         return prosumers[prosumer].reward;
-//     }
-
-//     function isProsumerRegistered(address prosumer) public view returns (bool) {
-//         return bytes(prosumers[prosumer].name).length > 0;
-//     }
-
-//     function deposit() public payable {
-//         require(msg.value > 0);
-//         contractBalance += msg.value;
-//         emit Deposited(msg.sender, msg.value);
-//     }
-
-//     uint256 public contractBalance;
-
-//     function withdraw() public {
-//         require(isProsumerRegistered(msg.sender), "Prosumer is not registered");
-//         Prosumer storage prosumer = prosumers[msg.sender];
-//         require(prosumer.balance > 0, "Insufficient balance to withdraw");
-//         uint256 amount = prosumer.balance;
-//         prosumer.balance = 0;
-//         payable(msg.sender).transfer(amount);
-//     }
-// }
--- test/p2_p.js
+++ test/p2_p.js
@@ -1,97 +1,99 @@
-// const P2P = artifacts.require("P2P");
+const P2P = artifacts.require("P2P");
 
-// /*
-//  * uncomment accounts to access the test accounts made available by the
-//  * Ethereum client
-//  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
-//  */
-// contract("P2P", async (/* accounts */) => {
-//   // P2P contract tests
-//   describe("P2P contract", async () => {
-//     // Arrange
-//     const p2pContract = await web3.eth.contract(P2P.info.abiDefinition).at("0x...");
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("P2P", async (/* accounts */) => {
+  // P2P contract tests
+  describe("P2P contract", async () => {
+    // Arrange
+    const p2pContract = await web3.eth.contract(P2P.info.abiDefinition).at("0x...");
 
-//     it("should be able to register a prosumer", async () => {
-//       // Act
-//       await p2pContract.registerProsumer("Test Name", 0);
+    it("should be able to register a prosumer", async () => {
+      // Act
+      await p2pContract.registerProsumer("Test Name", 0);
 
-//       // Assert
-//       const prosumer = await p2pContract.getProsumer(msg.sender);
-//       expect(prosumer.name).toEqual("Test Name");
-//       expect(prosumer.energyStatus).toEqual(0);
-//     });
+      // Assert
+      const prosumer = await p2pContract.getProsumer(msg.sender);
+      expect(prosumer.name).toEqual("Test Name");
+      expect(prosumer.energyStatus).toEqual(0);
+    });
 
-//     it("should be able to buy energy", async () => {
-//       // Arrange
-//       const buyer = await web3.eth.accounts.at("0x...");
-//       const seller = await web3.eth.accounts.at("0x...");
-//       const amount = 10;
-//       const price = 100;
+    it("should be able to buy energy", async () => {
+      // Arrange
+      const buyer = await web3.eth.accounts.at("0x...");
+      const seller = await web3.eth.accounts.at("0x...");
+      const amount = 10;
+      const price = 100;
 
-//       // Act
-//       await p2pContract.buyEnergy(amount, { from: buyer, value: amount * price });
+      // Act
+      await p2pContract.buyEnergy(amount, { from: buyer, value: amount * price });
 
-//       // Assert
-//       const buyerProsumer = await p2pContract.getProsumer(buyer);
-//       const sellerProsumer = await p2pContract.getProsumer(seller);
-//       expect(buyerProsumer.energyStatus).toEqual(energyStatus - amount);
-//       expect(sellerProsumer.energyStatus).toEqual(energyStatus + amount);
-//       expect(buyerProsumer.balance).toEqual(balance - amount * price);
-//       expect(sellerProsumer.balance).toEqual(balance + amount * price);
-//     });
+      // Assert
+      const buyerProsumer = await p2pContract.getProsumer(buyer);
+      const sellerProsumer = await p2pContract.getProsumer(seller);
+      expect(buyerProsumer.energyStatus).toEqual(energyStatus - amount);
+      expect(sellerProsumer.energyStatus).toEqual(energyStatus + amount);
+      expect(buyerProsumer.balance).toEqual(balance - amount * price);
+      expect(sellerProsumer.balance).toEqual(balance + amount * price);
+    });
 
-//     it("should be able to sell energy", async () => {
-//       // Arrange
-//       const buyer = await web3.eth.accounts.at("0x...");
-//       const seller = await web3.eth.accounts.at("0x...");
-//       const amount = 10;
-//       const price = 100;
+    it("should be able to sell energy", async () => {
+      // Arrange
+      const buyer = await web3.eth.accounts.at("0x...");
+      const seller = await web3.eth.accounts.at("0x...");
+      const amount = 10;
+      const price = 100;
 
-//       // Act
-//       await p2pContract.sellEnergy(amount, { from: seller, value: amount * price });
+      // Act
+      await p2pContract.sellEnergy(amount, { from: seller, value: amount * price });
 
-//       // Assert
-//       const buyerProsumer = await p2pContract.getProsumer(buyer);
-//       const sellerProsumer = await p2pContract.getProsumer(seller);
-//       expect(buyerProsumer.energyStatus).toEqual(energyStatus + amount);
-//       expect(sellerProsumer.energyStatus).toEqual(energyStatus - amount);
-//       expect(buyerProsumer.balance).toEqual(balance + amount * price);
-//       expect(sellerProsumer.balance).toEqual(balance - amount * price);
-//     });
+      // Assert
+      const buyerProsumer = await p2pContract.getProsumer(buyer);
+      const sellerProsumer = await p2pContract.getProsumer(seller);
+      expect(buyerProsumer.energyStatus).toEqual(energyStatus + amount);
+      expect(sellerProsumer.energyStatus).toEqual(energyStatus - amount);
+      expect(buyerProsumer.balance).toEqual(balance + amount * price);
+      expect(sellerProsumer.balance).toEqual(balance - amount * price);
+    });
 
-//     it("should not be able to buy energy if the buyer does not have enough ether", async () => {
-//       // Arrange
-//       const buyer = await web3.eth.accounts.at("0x...");
-//       const seller = await web3.eth.accounts.at("0x...");
-//       const amount = 10;
-//       const price = 100;
+    it("should not be able to buy energy if the buyer does not have enough ether", async () => {
+      // Arrange
+      const buyer = await web3.eth.accounts.at("0x...");
+      const seller = await web3.eth.accounts.at("0x...");
+      const amount = 10;
+      const price = 100;
 
-//       // Act
-//       try {
-//         await p2pContract.buyEnergy(amount, { from: buyer });
-//         fail("Should have failed");
-//       } catch (error) {
-//         expect(error.message).toEqual("Insufficient funds");
-//       }
-//     });
+      // Act
+      try {
+        await p2pContract.buyEnergy(amount, { from: buyer });
+        fail("Should have failed");
+      } catch (error) {
+        expect(error.message).toEqual("Insufficient funds");
+      }
+    });
 
-//     it("should not be able to sell energy if the seller does not have enough energy", async () => {
-//       // Arrange
-//       const buyer = await web3.eth.accounts.at("0x...");
-//       const seller = await web3.eth.accounts.at("0x...");
-//       const amount = 10;
-//       const price = 100;
+    it("should not be able to sell energy if the seller does not have enough energy", async () => {
+      // Arrange
+      const buyer = await web3.eth.accounts.at("0x...");
+      const seller = await web3.eth.accounts.at("0x...");
+      const amount = 10;
+      const price = 100;
 
-//       // Act
-//       try {
-//         await p2pContract.sellEnergy(amount, { from: seller });
-//         fail("Should have failed");
-//       } catch (error) {
-//         expect(error.message).toEqual("Insufficient energy");
-//       }
-//     });
-//   });
-// });
+      // Act
+      try {
+        await p2pContract.sellEnergy(amount, { from: seller });
+        fail("Should have failed");
+      } catch (error) {
+        expect(error.message).toEqual("Insufficient energy");
+      }
+    });
+  });
+});
+
+// Code above this is first-or-final-working-draft.
 
 // const { assert } = require('chai');
 
