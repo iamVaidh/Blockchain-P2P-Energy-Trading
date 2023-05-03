a591d42937f5db51984d85c527c22ccc84264abd
Vaidh <1999prithviraj1999@gmail.com>
2023-05-03 20:12:29 +0100

    Code refactoring

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -25,6 +25,10 @@ contract Main {
         p2pContract = P2P(_p2pContractAddress);
     }
 
+    function registerProsumer(string memory _name, uint _energyStatus) public onlyOnce {
+        p2pContract.registerProsumer(_name, _energyStatus);
+    }
+    
     function isRegistered(address prosumer) public view returns (bool) {
         return p2pContract.isProsumerRegistered(prosumer);
     }
@@ -36,12 +40,17 @@ contract Main {
         return p2pContract.getBalance(prosumer) >= amount;
     }
 
-    function registerProsumer(string memory _name, uint _energyStatus) public onlyOnce {
-        p2pContract.registerProsumer(_name, _energyStatus);
-    }
-
     function depositEthers() public payable onlyRegistered {
         p2pContract.deposit{value: msg.value}();
+    }
+
+    function getBalance(address prosumer) public view returns (uint) {
+        return p2pContract.getBalance(prosumer);
+    } 
+
+    function withdrawEthers() public payable onlyRegistered {
+        require(getEnergyStatus(msg.sender) >= 0);
+        p2pContract.withdraw();
     }
 
     function sendEnergyRequest(
@@ -57,15 +66,6 @@ contract Main {
 
     function getEnergyStatus(address prosumer) public view returns (uint) {
         return p2pContract.getEnergyStatus(prosumer);
-    }
-
-    function getBalance(address prosumer) public view returns (uint) {
-        return p2pContract.getBalance(prosumer);
-    } 
-
-    function withdrawEthers() public payable onlyRegistered {
-        require(getEnergyStatus(msg.sender) >= 0);
-        p2pContract.withdraw();
     }
 
     function setEnergyPrice(uint256 price) public onlyRegistered {
